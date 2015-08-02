184185b0eed94cb1aa8c4f869fa52ccba245b432
Jose Augusto Fioruci <jafioruci@gmail.com>
2015-08-02 08:23:14 +0000

    version 1.1

--- R/forecastFunctions.R
+++ R/forecastFunctions.R
@@ -1,16 +1,34 @@
-otm <- function( y, h=5, seasonal=NULL, theta=NULL, g="sAPE", n1=length(y)-h, 
-						m=floor(h/2), H=h, p=1+floor((length(y)-n1)/m),
-						thetaList=seq(from=1,to=5,by=0.5), tLineExtrap=ses, 
-						mc.cores=1, ...){
-
+otm <- function( y, h=5, seasonal=NULL, theta=NULL, tLineExtrap=ses, g="sAPE",
+		approach="c",
+		n1=NULL, m=NULL, H=NULL, p=NULL,
+		thetaList=seq(from=1,to=5,by=0.5), 
+		mc.cores=1, ...){
+	
 	if(!is.ts(y)){ stop("ERROR in otm function: y must be a object of time series class.") }
 	if(!is.numeric(h)){	stop("ERROR in otm function: h must be a positive integer number.")	}
 	if(!is.null(theta) && !is.numeric(theta)){ stop("ERROR in otm function: theta must be a numeric value higher or equal to 1 or NULL.")}
 	if(is.numeric(theta) && theta < 1){	stop("ERROR in otm function: theta must be a numeric value higher or equal to 1 or NULL.")}
+	if( !is.null(approach) && !is.null(c(n1,m,H,p)) ){stop("ERROR in otm function: do approach=NULL if you want to set a new approach for groe parameters.")}
+	n = length(y)
+	if(is.null(theta) && !is.null(approach)){
+		if( any(approach == c('a','b','c','d','e','f','g','h')) ){
+			if(approach == 'a'){n1=n-h; m=h; H=h;p=1;}
+			if(approach == 'b'){n1=n-h; m=floor(h/2); H=h; p=2;}
+			if(approach == 'c'){n1=n-h; m=floor(h/3); H=h; p=3;}
+			if(approach == 'd'){n1=n-h; m=1; H=h; p=h;}	
+			if(approach == 'e'){n1=n-2*h; m=h; H=h; p=2;}
+			if(approach == 'f'){n1=n-2*h; m=floor(h/2); H=h; p=4;}
+			if(approach == 'g'){n1=n-2*h; m=floor(h/3); H=h; p=6;}
+			if(approach == 'h'){n1=n-2*h; m=1; H=h; p=h;}
+			if(n1 < 4){n1=4;}
+		}else{
+			stop("ERROR in otm function: the argument 'approach' must lie between 'a' and 'h')")
+		}
+	}
 	if(is.null(theta) && n1 < 4) stop("ERROR in otm function: n1 < 4)") 
 	if(is.null(theta) && p > 1+floor((length(y)-n1)/m)){ stop("ERROR in otm function: p > 1+floor((length(y)-n1)/m)") }
 	
-	n = length(y)
+	
 	fq = frequency(y)
 	time_y = time(y)
 	
@@ -69,6 +87,9 @@ otm <- function( y, h=5, seasonal=NULL, theta=NULL, g="sAPE", n1=length(y)-h,
     fit$fitted = Y_fitted
     fit$residuals = Y_residuals
 	fit$theta = theta
+	if(!is.null(X$model$par)){
+		fit$tLineExtrap_par = X$model$par
+	}
 	fit$weights = c(omega, 1-omega)
 	
 	return(fit)
